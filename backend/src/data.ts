754cbab354595c3e451f725979f157447c7430a4
PasserBo <bobo.yiyangxu@gmail.com>
2025-03-17 18:45:44 -0500

    Fixed type error

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -4,4 +4,4 @@ import { Restaurant,EmojiData } from "./types";
 const dataPath = "../Data_Restaurants/all_restaurants.json";
 const emojiPath = "../Data_Restaurants/emoji.json";
 export const restaurants: Restaurant[] = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
-export const emojiData: EmojiData[] = JSON.parse(fs.readFileSync(emojiPath, "utf-8"));
+export const emojiData: EmojiData[] = JSON.parse(fs.readFileSync(emojiPath, "utf-8")).foodEmojis;
